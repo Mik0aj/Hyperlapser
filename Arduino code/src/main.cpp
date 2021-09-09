701a0dd8142bf522ba9fce6b6e7c6577596b4956
Mikołaj Aleksander Mosoń <mikoajmoson@gmail.com>
2021-09-09 23:30:34 +0200

    Finally managed to put object functions into array, now buttons and corresponding functions are called using for loop.

--- Arduino code/src/main.cpp
+++ Arduino code/src/main.cpp
@@ -3,15 +3,18 @@
 #include <Hyperlapser.h>
 #include "Display.h"
 
+
 const int BAUD_RATE = 9600;
 const int COMMON_PIN = 2;
 const int UP_BUTTON = 4;
 const int DOWN_BUTTON = 5;
 const int CONFIRM_BUTTON = 6;
+const int BUTTON_ARRAY[]={UP_BUTTON,DOWN_BUTTON,CONFIRM_BUTTON};
+void (Hyperlapser::*functions[])()={&Hyperlapser::upButton,&Hyperlapser::downButton,&Hyperlapser::confirmButton};
 unsigned long lastFire = 0;
+int test=1;
 Hyperlapser hyperlapser;
 Display display(0x27, 16, 2);
-
 void setup() {
     Serial.begin(BAUD_RATE);
     pinMode(UP_BUTTON, INPUT_PULLUP);
@@ -23,19 +26,13 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
+    if(test==5)test=1;
     if (millis() - lastFire > 200) { // Debounce
-        if (!digitalRead(UP_BUTTON)) {
-            hyperlapser.upButton();
-        }
-        if (!digitalRead(DOWN_BUTTON)) {
-            hyperlapser.downButton();
-        }
-        if (!digitalRead(CONFIRM_BUTTON)) {
-            hyperlapser.confirmButton();
+        for(int i=0;i<3;i++){
+            if (!digitalRead(BUTTON_ARRAY[i])) {
+                (hyperlapser.*functions[i])();
+            }
         }
         lastFire = millis();
     }
-    delay(3000);
-    hyperlapser.notify();
-    hyperlapser.setCurrentMenu(1);
 }
