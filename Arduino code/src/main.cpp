7e5126976f089a23b1fc6e9925d519bf68f9e050
Mikołaj Aleksander Mosoń <mikoajmoson@gmail.com>
2021-09-14 22:14:06 +0200

    added servo to main, loop now moves servo when hyperlapser changes state to running,

--- Arduino code/src/main.cpp
+++ Arduino code/src/main.cpp
@@ -1,18 +1,21 @@
 #include <Arduino.h>
 #include <Hyperlapser.h>
 #include "Display.h"
+#include "Servo.h"
 
 
 const int BAUD_RATE = 9600;
 const int UP_BUTTON = 4;
 const int DOWN_BUTTON = 5;
 const int CONFIRM_BUTTON = 6;
-const int BUTTON_ARRAY[]={UP_BUTTON,DOWN_BUTTON,CONFIRM_BUTTON};
-void (Hyperlapser::*functions[])()={&Hyperlapser::upButton,&Hyperlapser::downButton,&Hyperlapser::confirmButton};
-unsigned long lastFire = 0;
-int test=1;
+const int BUTTON_ARRAY[] = {UP_BUTTON, DOWN_BUTTON, CONFIRM_BUTTON};
+Servo servo;
+void (Hyperlapser::*functions[])() ={&Hyperlapser::upButton, &Hyperlapser::downButton, &Hyperlapser::confirmButton};
+unsigned long lastButtonClick = 0;
+unsigned long lastServoMove = 0;
 Hyperlapser hyperlapser;
-Display display(0x27, 16, 2,&hyperlapser);
+Display display(0x27, 16, 2, &hyperlapser);
+
 void setup() {
     Serial.begin(BAUD_RATE);
     pinMode(UP_BUTTON, INPUT_PULLUP);
@@ -20,17 +23,21 @@ void setup() {
     pinMode(CONFIRM_BUTTON, INPUT_PULLUP);
     display.initClass();
     hyperlapser.registerObserver(&display);
+    servo.attach(8, 500, 2500);
+    hyperlapser.setServo(&servo);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-    if(test==5)test=1;
-    if (millis() - lastFire > 200) { // Debounce
-        for(int i=0;i<3;i++){
+    if (millis() - lastButtonClick > 200) { // Debounce
+        for (int i = 0; i < 3; i++) {
             if (!digitalRead(BUTTON_ARRAY[i])) {
                 (hyperlapser.*functions[i])();
             }
         }
-        lastFire = millis();
+        lastButtonClick = millis();
+    } else if (millis() - lastServoMove >= hyperlapser.getInterval() * 1000 && hyperlapser.isRunning()) {
+        lastServoMove = millis();
+        hyperlapser.moveServo();
     }
 }
