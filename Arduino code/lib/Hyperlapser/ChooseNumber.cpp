3cae3c2e8cfff0ae936de7e98d3a88fa55fb8ab5
Mikołaj Aleksander Mosoń <mikoajmoson@gmail.com>
2021-09-14 19:51:41 +0200

    added logic to Choose Number State

--- Arduino code/lib/Hyperlapser/ChooseNumber.cpp
+++ Arduino code/lib/Hyperlapser/ChooseNumber.cpp
@@ -1,67 +1,82 @@
 ////
 //// Created by mikoaj on 05.09.2021.
 ////
-//
-//
-//void ChooseNumber::upButton() {
-//    hyperlapser.setCurrentNumber(hyperlapser.getCurrentNumber + 1);
-//
-//}
-//
-//void ChooseNumber::downButton() {
-//    hyperlapser.setCurrentNumber(hyperlapser.getCurrentNumber -1 );
-//}
-//
-//void ChooseNumber::confirmButton() {
-//    currentNumber = hyperlapser.getCurrentNumber();
-//    String newNumber = String(number);
-//    int number;
-//    switch (currentNumber) {
-//        case REMOVE_NUMBER:
-//            newNumber.remove(newNumber.length() - 1);
-//            break;
-//        case CONFIRM_NUMBER:
-//            number = newNumber.toInt();
-//            switch (hyperlapser.getCurrentMenu) {
-//                case TIME:
-//                    hyperlapser.setTime(number);
-//                    break;
-//                case SET_START_POSITION:
-//                    hyperlapser.setStartPosition(number);
-//                    break;
-//                case SET_END_POSITION:
-//                    hyperlapser.setEndPosition(number);
-//                    break;
-//            }
-//            hyperlapser.setHyperlapserState(hyperlapser.getMenuState());
-//            break;
-//        case default:
-//            newNumber.concat(numberMenu);
-//            break;
-//    }
-//}
-//
-//ChooseNumber::ChooseNumber(Hyperlapser *newHyperlapser) {
-//    hyperlapser = newHyperlapser;
-//}
-#include "ChooseNumber.h"
-#include "Menu.h"
 
-ChooseNumber::ChooseNumber(Hyperlapser* newOwner):State(owner){
+#include "ChooseNumber.h"
+
+ChooseNumber::ChooseNumber(Hyperlapser *newOwner) : State(owner) {
 
 }
-void ChooseNumber::confirmButton() {
-    owner->changeStateToMenu();
 
+void ChooseNumber::confirmButton() {
+    String newNumber = String(owner->getNumber());
+    switch (owner->getCurrentNumber()) {
+        case 0:
+            newNumber.concat(0);
+            break;
+        case 1:
+            newNumber.concat(1);
+            break;
+        case 2:
+            newNumber.concat(2);
+            break;
+        case 3:
+            newNumber.concat(3);
+            break;
+        case 4:
+            newNumber.concat(4);
+            break;
+        case 5:
+            newNumber.concat(5);
+            break;
+        case 6:
+            newNumber.concat(6);
+            break;
+        case 7:
+            newNumber.concat(7);
+            break;
+        case 8:
+            newNumber.concat(8);
+            break;
+        case 9:
+            newNumber.concat(9);
+            break;
+        case ConstantValues::REMOVE_NUMBER:
+            newNumber.remove(newNumber.length() - 1);
+            break;
+        case ConstantValues::CONFIRM_NUMBER:
+            switch (owner->getCurrentMenu()) {
+                case ConstantValues::CHANGE_TIME:
+                    owner->setTime(owner->getNumber());
+                    break;
+                case ConstantValues::SET_START_POSITION:
+                    owner->setStartPos(owner->getNumber());
+                    break;
+                case ConstantValues::SET_END_POSITION:
+                    owner->setEndPos(owner->getNumber());
+                    break;
+            }
+            owner->setNumber(0);
+            owner->changeStateToMenu();
+            owner->setCurrentNumber(0);
+            return;
+    }
+    owner->setNumber(newNumber.toInt());
 }
 
 void ChooseNumber::downButton() {
-    Serial.print("downButton ChooseNumber ");
+    owner->numberDown();
 
 }
 
 void ChooseNumber::upButton() {
-    Serial.print("upbutton ChooseNumber ");
+    owner->numberUp();
+    Serial.print(owner->getCurrentNumber());
+    Serial.println(owner->getCurrentMenu());
 
+}
+
+int ChooseNumber::returnStateId() {
+    return ConstantValues::CHOOSE_NUMBER_STATE;
 }
 
--- Arduino code/lib/Hyperlapser/ChooseNumber.h
+++ Arduino code/lib/Hyperlapser/ChooseNumber.h
@@ -19,6 +19,7 @@ public:
 
     void confirmButton() override;
 
+    int returnStateId();
 };
 
 
